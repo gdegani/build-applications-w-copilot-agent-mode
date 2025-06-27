f3a25fad64d5c7d8b03ef4d333aaf55cb454dfa2
Giancarlo Degani <giancarlo@degani.eu>
2025-06-27 21:44:25 +0000

    test

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -3,6 +3,23 @@ from rest_framework import viewsets
 from .models import User, Team, Activity, Workout, Leaderboard
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, WorkoutSerializer, LeaderboardSerializer
 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    # Use both Codespace and localhost URLs for dev/test
+    codespace_url = 'https://stunning-capybara-vpjj45qj5r2xg5-8000.app.github.dev/'
+    localhost_url = 'http://localhost:8000/'
+    return Response({
+        'users': [codespace_url + 'api/users/', localhost_url + 'api/users/'],
+        'teams': [codespace_url + 'api/teams/', localhost_url + 'api/teams/'],
+        'activity': [codespace_url + 'api/activity/', localhost_url + 'api/activity/'],
+        'workouts': [codespace_url + 'api/workouts/', localhost_url + 'api/workouts/'],
+        'leaderboard': [codespace_url + 'api/leaderboard/', localhost_url + 'api/leaderboard/'],
+    })
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
